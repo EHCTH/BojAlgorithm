98c0da4f5f2ac40755cc56133b93c310df92bd51
EHCTH <111932212+EHCTH@users.noreply.github.com>
2024-09-20 21:11:40 +0900

    Add new file

---
+++ IMPLEMENTATION/2752.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        Arrays.stream(arr).forEach((x) -> System.out.print(x + " "));
+    }
+}
+
+
+class Coordinate {
+    int first;
+    int second;
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public Coordinate(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return first + " " + second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Coordinate) {
+            Coordinate coordinate = (Coordinate) obj;
+            return first == coordinate.first && second == coordinate.second;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+}
