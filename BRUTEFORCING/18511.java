43ae82b7045909e9a1d383fa149e0d41e4e35e6b
EHCTH <111932212+EHCTH@users.noreply.github.com>
2024-09-20 21:14:18 +0900

    Add new file

---
+++ BRUTEFORCING/18511.java
@@ -0,0 +1,194 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+        /*
+
+          +---+
+         /   /|
+        +---+ |
+        |   | +
+        |   |/
+        +---+
+
+        */
+
+public class Main {
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String nextToken() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(bf.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(nextToken());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(nextToken());
+    }
+    static int N, K;
+    static String[] arr = new String[3];
+    static int maxValue = Integer.MIN_VALUE;
+    static int length;
+    static void init() throws IOException {
+        N = nextInt();
+        K = nextInt();
+        length = String.valueOf(N).length();
+        for (int i = 0; i < K; i++) {
+            arr[i] = nextToken();
+        }
+
+    }
+    static int transformIntegerAtString(Stack<String> answer) {
+        if (answer.isEmpty()) return 0;
+        String s = "";
+        for (String a : answer) {
+            s += a;
+        }
+        return Integer.parseInt(s);
+    }
+    static void permutation(int n, Stack<String> answer) {
+        if (n > 0) {
+            int a = transformIntegerAtString(answer);
+            if (n > length || a > N) return;
+            maxValue = Math.max(maxValue, a);
+        }
+        for (int i = 0; i < K; i++) {
+            answer.push(arr[i]);
+            permutation(n  + 1, answer);
+            answer.pop();
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        init();
+        permutation(0, new Stack<>());
+        System.out.println(maxValue);
+    }
+}
+
+
+class Node implements Comparable<Node> {
+    int u;
+    int v;
+    int w;
+
+    Node(int u, int v, int w) {
+        this.u = u;
+        this.v = v;
+        this.w = w;
+    }
+
+    void swap() {
+        int tmp = u;
+        u = v;
+        v = tmp;
+    }
+
+    boolean isConnected() {
+        return u == v;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(u, v, w);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Node) {
+            Node node = (Node) obj;
+            return u == node.u && v == node.v && w == node.w;
+        }
+        return false;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return Integer.compare(w, o.w);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(u).append(" ").append(v).append(" ").append(w);
+        return sb.toString();
+    }
+}
+
+class Coordinate implements Comparable<Coordinate> {
+    int first;
+    int second;
+    int third;
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public Coordinate(int first, int second, int third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public Coordinate(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return first + " " + second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof Coordinate) {
+            Coordinate coordinate = (Coordinate) obj;
+            return first == coordinate.first && second == coordinate.second;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+
+    /*
+
+     왼쪽이 클 경우 양수
+     오른 쪽이 클 경우 음수
+     같을 경우 0
+
+     */
+
+    //     first 오름차순 정렬 후 값이 같을경우 second 오름 차 순 정렬
+    @Override
+    public int compareTo(Coordinate o) {
+        int firstSort = Integer.compare(first, o.first);
+        if (firstSort == 0) {
+            return Integer.compare(second, o.second);
+        }
+        return firstSort;
+    }
+}
