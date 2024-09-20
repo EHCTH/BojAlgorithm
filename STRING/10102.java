9efdd96af744dc3ae5391fd9a6f0b965507287d1
EHCTH <111932212+EHCTH@users.noreply.github.com>
2024-09-20 21:52:38 +0900

    Add new file

---
+++ STRING/10102.java
@@ -0,0 +1,150 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.*;
+
+        /*
+
+          +---+
+         /   /|
+        +---+ |
+        |   | +
+        |   |/
+        +---+
+
+        */
+
+public class Main {
+    static int n;
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static int[] answer = new int[2];
+
+    static void init() throws Exception {
+        n = Integer.parseInt(bf.readLine());
+        String st = bf.readLine();
+        for (int i = 0; i < n; i++) {
+            answer[change(st.charAt(i))]++;
+        }
+    }
+    static int change(char c) {
+        if (c == 'A') return 0;
+        return 1;
+    }
+    public static void main(String[] args) throws Exception{
+        init();
+        if (answer[0] == answer[1]) System.out.println("Tie");
+        else if (answer[0] > answer[1]) System.out.println('A');
+        else System.out.println('B');
+    }
+}
+class Node implements Comparable<Node> {
+    int u;
+    int v;
+    int w;
+
+    Node(int u, int v, int w) {
+        this.u = u;
+        this.v = v;
+        this.w = w;
+    }
+
+    void swap() {
+        int tmp = u;
+        u = v;
+        v = tmp;
+    }
+
+    boolean isConnected() {
+        return u == v;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(u, v, w);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Node) {
+            Node node = (Node) obj;
+            return u == node.u && v == node.v && w == node.w;
+        }
+        return false;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return Integer.compare(w, o.w);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(u).append(" ").append(v).append(" ").append(w);
+        return sb.toString();
+    }
+}
+
+class Coordinate implements Comparable<Coordinate> {
+    int first;
+    int second;
+    int third;
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public Coordinate(int first, int second, int third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public Coordinate(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return first + " " + second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof Coordinate) {
+            Coordinate coordinate = (Coordinate) obj;
+            return first == coordinate.first && second == coordinate.second;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+
+    /*
+
+     왼쪽이 클 경우 양수
+     오른 쪽이 클 경우 음수
+     같을 경우 0
+
+     */
+
+    //     first 오름차순 정렬 후 값이 같을경우 second 오름 차 순 정렬
+    @Override
+    public int compareTo(Coordinate o) {
+        int firstSort = Integer.compare(first, o.first);
+        if (firstSort == 0) {
+            return Integer.compare(second, o.second);
+        }
+        return firstSort;
+    }
+}
